bf4733464c361974027034522b82c0ca7582ffae
8bitdev256 <157895971+8bitdev256@users.noreply.github.com>
2024-07-28 20:25:00 -0300

    Working on calendar implementation

--- hello-world/src/App.vue
+++ hello-world/src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
     <HelloWorld msg="Agenda"/>
+    <Counter/>
+    <FullCalendar :options='calendarOptions' />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import FullCalendar from '@fullcalendar/vue';
+import dayGridPlugin from '@fullcalendar/daygrid';
 import HelloWorld from './components/HelloWorld.vue';
+import Counter from './components/Counter.vue';
 
 @Component({
   components: {
     HelloWorld,
+    Counter,
+    FullCalendar,
   },
+  data: () => ({
+    calendarOptions: {
+      plugins: [dayGridPlugin],
+      initialView: 'dayGridMonth',
+      weekends: false,
+      events: [
+        { title: 'Meeting', start: new Date() },
+      ],
+    },
+  }),
 })
+
 export default class App extends Vue {}
 </script>
 
---
+++ hello-world/src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button v-on:click="decrement">-</button>
+        {{ count }}
+        <button v-on:click="increment">+</button>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Component from 'vue-class-component';
+
+@Component
+export default class Counter extends Vue {
+    count = 0
+
+    decrement() {
+      this.count -= 1;
+    }
+
+    increment() {
+      this.count += 1;
+    }
+}
+</script>
